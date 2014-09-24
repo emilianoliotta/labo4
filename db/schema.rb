56efb60b3f126021f67bc145730b9ceaccecda5f
Emiliano Liotta <liotta.emiliano@gmail.com>
2014-09-24 15:19:37 -0300

    Con PG funcionando

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140904131821) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "monstruos", force: true do |t|
     t.string   "nombre"
     t.datetime "created_at"
